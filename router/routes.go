25f250cfb0edd6b26a26d6fef7368ab20978c266
Luiz Henrique <luizhjramos@outlook.com>
2023-03-25 13:22:39 -0300

    feat: add routes logic

--- router/routes.go
+++ router/routes.go
@@ -14,5 +14,25 @@ func initializeRoutes(router *gin.Engine) {
 				"msg": "GET Opening API",
 			})
 		})
+		v1.GET("/openings", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET Openings API",
+			})
+		})
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "POST Opening API",
+			})
+		})
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "DELETE Opening API",
+			})
+		})
+		v1.PUT("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "PUT Opening API",
+			})
+		})
 	}
 }
