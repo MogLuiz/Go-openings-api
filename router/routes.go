e17c2fdcb6605f98b671e4834481d0461b9daf6d
Luiz Henrique <luizhjramos@outlook.com>
2023-03-25 14:03:25 -0300

    refactor: separate files for route handlers

--- router/routes.go
+++ router/routes.go
@@ -9,7 +9,7 @@ func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("api/v1")
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
-		v1.GET("/openings", handler.ListOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningsHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
