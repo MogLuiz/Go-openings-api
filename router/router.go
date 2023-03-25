1c331dfa9eeec3e1d78512ea8dd115ff3b032da0
Luiz Henrique <luizhjramos@outlook.com>
2023-03-25 13:14:07 -0300

    refactor: abstract routes initialization to own group

--- router/router.go
+++ router/router.go
@@ -3,11 +3,12 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	// Initialize Router
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run(":8080") // listen and serve on 0.0.0.0:8080
+
+	// Initialize Routes
+	initializeRoutes(router)
+
+	// Run the server
+	router.Run(":8080")
 }
---
+++ router/routes.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initializeRoutes(router *gin.Engine) {
+	v1 := router.Group("api/v1")
+	{
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET Opening API",
+			})
+		})
+	}
+}
